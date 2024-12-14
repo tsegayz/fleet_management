4c2efb5851f302239ec6a88b268dc289c75a9b6d
Tsega Yaekob <tsegayaekob7@gmail.com>
2024-12-14 15:28:40 +0300

    origins for backend

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,19 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(json());
 const corsOptions = {
-	origin: "http://localhost:5173", 
-};
-
+	origin: (origin, callback) => {
+	  const allowedOrigins = [
+		"http://localhost:5173",
+		"https://fleet-management-iota.vercel.app"
+	  ];
+  
+	  if (!origin || allowedOrigins.includes(origin)) {
+		callback(null, true);
+	  } else {
+		callback(new Error("Not allowed by CORS"));
+	  }
+	}
+  };
 app.use(cors(corsOptions));
 
 
